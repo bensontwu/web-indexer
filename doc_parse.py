eff482442f06fbb6ab9c8dceced02951decf4cfe
benson <bensontwu@benson-mbp.local>
2021-02-10 18:21:48 -0800

    added getting html and url from json

--- doc_parse.py
+++ doc_parse.py
@@ -7,3 +7,9 @@ def get_json_from_file(file_name: str) -> dict:
 def dump_json_to_file(json_dict: dict, file_name: str) -> dict:
     with open(file_name, "w") as f:
         json.dump(json_dict, f)
+
+def get_url_from_json(json_dict: dict) -> str:
+    return json_dict['url']
+
+def get_html_content_from_json(json_dict: dict) -> str:
+    return json_dict['content']
