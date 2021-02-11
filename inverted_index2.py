13e55d776d429d037a370269938d45c5d7f24ef0
benson <bensontwu@benson-mbp.local>
2021-02-10 20:07:33 -0800

    handle jsondecode error

--- inverted_index2.py
+++ inverted_index2.py
@@ -1,6 +1,7 @@
 from utils import get_file_names
 from doc_parse import get_json_from_file, get_html_content_from_json, get_url_from_json, dump_json_to_file
 from tokenizer import tokenize, compute_word_frequencies
+from json.decoder import JSONDecodeError
 import sys
 import psutil
 
@@ -24,24 +25,27 @@ def main(directory_name):
     doc_id = 0
     file_names = get_file_names(directory_name)
     for file in file_names:
-        # add the term to the inverted index
-        json_dict = get_json_from_file(file)
-        html_content = get_html_content_from_json(json_dict)
-        tokens = tokenize(html_content)
-        word_freqs = compute_word_frequencies(tokens)
-        create_term(inverted_index, word_freqs, doc_id)
-        
-        # add the url and document id mapping to the doc map
-        url = get_url_from_json(json_dict)
-        doc_map[doc_id] = url
-        doc_id += 1
+        try:
+            # add the term to the inverted index
+            json_dict = get_json_from_file(file)
+            html_content = get_html_content_from_json(json_dict)
+            tokens = tokenize(html_content)
+            word_freqs = compute_word_frequencies(tokens)
+            create_term(inverted_index, word_freqs, doc_id)
+            
+            # add the url and document id mapping to the doc map
+            url = get_url_from_json(json_dict)
+            doc_map[doc_id] = url
+            doc_id += 1
 
-        # offload the index if need be
-        if time_to_offload():
-            dump_json_to_file(inverted_index, get_partial_index_file_name(partial_index_count))
-            inverted_index = {}; partial_index_count += 1
-    
-        print(f"Completed file: {file}")
+            # offload the index if need be
+            if time_to_offload():
+                dump_json_to_file(inverted_index, get_partial_index_file_name(partial_index_count))
+                inverted_index = {}; partial_index_count += 1
+            
+            print(f"Completed file: {file}")
+        except JSONDecodeError:
+            continue
 
     # offload the index
     dump_json_to_file(inverted_index, get_partial_index_file_name(partial_index_count))
