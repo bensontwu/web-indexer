1c4840e10b2d6fea0fd82e8db7f1f948aea74b14
benson <bensontwu@benson-mbp.local>
2021-02-17 19:05:09 -0800

    Added basic search function

---
+++ posting.py
@@ -0,0 +1,50 @@
+# Get list of postings from index
+def get_postings(index, query_term) -> list:
+    try:
+        return index[query_term]
+    except KeyError:
+        return []
+
+# Creates a dictionary of postings
+# for quicker intersection
+# { doc_id, token_frequency }
+
+
+def get_postings_dict(postings_list) -> dict:
+    p_dict = {}
+    for posting in postings_list:
+        p_dict[posting[0]] = posting[1]
+    return p_dict
+
+# Sorts list of postings
+
+
+def get_sorted_postings(postings_dict: dict) -> dict:
+    return dict(sorted(postings_dict.items(), key=lambda posting: -posting[1]))
+
+# # O(N)
+# def doc_in_postings(doc_id: int, postings: list):
+#     for posting in postings:
+#         if doc_id == posting[0]:
+#             return True
+#     return False
+
+# Creates combined dictionary of postings
+
+
+def get_combined_postings(l_postings: dict, r_postings: dict) -> dict:
+    final_postings = {}
+    for doc_id in l_postings.keys():
+        if doc_id in r_postings.keys():
+            # add the frequencies together to get a combined token frequency for sorting
+            final_postings[doc_id] = l_postings[doc_id] + r_postings[doc_id]
+    return final_postings
+
+# Self explainatory
+
+
+def print_postings(postings_dict: dict, limit: int = None) -> None:
+    for i, (doc_id, token_freq) in enumerate(postings_dict.items()):
+        if limit != None and i >= limit:
+            break
+        print(f"{doc_id} with a freq of {token_freq}")
---
+++ search.py
@@ -0,0 +1,34 @@
+import sys
+
+from doc_parse import get_json_from_file
+from posting import get_combined_postings, get_postings, get_postings_dict, get_sorted_postings, print_postings
+
+# Input loop
+if __name__ == "__main__":
+    index_file_name = sys.argv[1]
+    index = get_json_from_file(index_file_name)
+
+    while True:
+        query_string = input("Please search me!\n>>> ")
+        query_terms = query_string.split()
+
+        # get matching postings from each query term
+        posting_dicts = []
+        for query_term in query_terms:
+            postings = get_postings(index, query_term)
+            p_dict = get_postings_dict(postings)
+            posting_dicts.append(p_dict)
+        
+        # get the combined postings from the multiple terms i.e. AND operation
+        combined_postings = posting_dicts[0]
+        for p_dict in posting_dicts[1:]:
+            combined_postings = get_combined_postings(combined_postings, p_dict)
+        
+        # sort the postings
+        sorted_postings = get_sorted_postings(combined_postings)
+
+        # print the sorted postings
+        print_postings(sorted_postings)
+        
+
+
