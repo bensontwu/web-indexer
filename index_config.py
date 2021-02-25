ff5bb22f697de1a0e7053639d5e4cf112bb5b240
Benson Wu <40438500+bensontwu@users.noreply.github.com>
2021-02-24 23:46:18 -0800

    refactored inverted index to be a class (#2)

---
+++ index_config.py
@@ -0,0 +1,41 @@
+import configparser
+
+
+class IndexConfig:
+    def __init__(self, config_file_name):
+        config = configparser.ConfigParser()
+        config.read(config_file_name)
+
+        index_file_names = config['INDEX_FILE_NAMES']
+        partial_index_file_names = config['PARTIAL_INDEX_FILE_NAMES']
+        index_location = config['INDEX_LOCATION']
+        documents_location = config['DOCUMENTS_LOCATION']
+
+        self._input_dir_name = documents_location['INPUT_DIR']
+        self._output_dir_name = index_location['OUTPUT_DIR']
+        self._index_file_name = index_file_names['INVERTED_INDEX']
+        self._token_locator_file_name = index_file_names['TOKEN_LOCATOR']
+        self._doc_id_map = index_file_names['DOC_ID_MAP']
+        self._partial_index_base_file_name = partial_index_file_names['INDEX_BASE']
+        self._partial_index_token_locator_file_name = partial_index_file_names['TOKEN_LOCATOR']
+
+    def get_output_dir(self) -> str:
+        return self._output_dir_name
+
+    def get_input_dir(self) -> str:
+        return self._input_dir_name
+
+    def get_index_file_path(self) -> str:
+        return self._output_dir_name + "/" + self._index_file_name
+
+    def get_token_locator_path(self) -> str:
+        return self._output_dir_name + "/" + self._token_locator_file_name
+
+    def get_doc_id_map_path(self) -> str:
+        return self._output_dir_name + "/" + self._doc_id_map
+
+    def get_partial_index_base_path(self) -> str:
+        return self._output_dir_name + "/" + self._partial_index_base_file_name
+
+    def get_partial_index_token_locator_path(self) -> str:
+        return self._output_dir_name + "/" + self._partial_index_token_locator_file_name
