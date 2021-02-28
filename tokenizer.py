fdc5cb15a129c9a66a7535bbbd7662707f67596e
Yu <hatomugi_yu@users.noreply.github.com>
2021-02-28 12:02:17 +0900

    strong and title count added

--- tokenizer.py
+++ tokenizer.py
@@ -5,13 +5,11 @@ from bs4.element import Comment
 from bs4 import BeautifulSoup
 nltk.stem.PorterStemmer()
         
-def tokenize(html) -> list:
-    s = nltk.stem.PorterStemmer()
-    final_tokens = []
-    soup = BeautifulSoup(html, 'html.parser')
 
-    texts = soup.findAll(text=True)
-    visible_texts = filter(_elem_check, texts)
+def tokenize_text(visible_texts) -> list:
+
+    final_tokens = []
+
     for text in visible_texts:
         line = text.lower()
         tokens = re.split("[^A-Za-z0-9']", line)
@@ -19,12 +17,47 @@ def tokenize(html) -> list:
             stemmed_word = s.stem(tok)
             if stemmed_word != "":
                 final_tokens.append(stemmed_word)
+
     return final_tokens
 
-def compute_word_frequencies(tokens):
+
+def tokenize(html) -> list:
+    s = nltk.stem.PorterStemmer()
+    final_tokens = []
+    soup = BeautifulSoup(html, 'html.parser')
+
+    # Get texts of the content and tokenize it -------------------
+    texts = soup.findAll(text=True)
+    visible_texts = filter(_elem_check, texts)
+    
+    content_tokens = tokenize_text(visible_texts)
+
+    # Get texts of the strong tag and tokenize it ----------------
+    strong_lst = html.findAll("strong")
+    strong_lst = [i.get_text() for i in strong_lst]
+
+    # All text within strong tag
+    strong_text = ' '.join(strong_lst)
+    strong_tokens = tokenize_text(strong_text)
+
+    # All text within title tag ----------------------------------
+    title_text = html.find("title").get_text()
+    title_tokens = tokenize_text(title_text)
+
+    return final_tokens, strong_tokens, title_tokens
+
+
+def compute_word_frequencies(tokens, strong_tokens, title_tokens):
     final_dict = defaultdict(int)
     for tok in tokens:
         final_dict[tok] += 1
+
+    for tok in strong_tokens:
+        final_dict[tok] += 1
+
+    for tok in title_tokens:
+        final_dict[tok] += 2
+
     return final_dict
 
 # This function return true
